5117976ff28c380b9b91ff636d883b7818888dcb
N DeBliek <ndebl589@mtroyal.ca>
2024-09-29 18:46:05 -0600

    fixed missplaced letter in inventory manager #6

--- scripts/main.py
+++ scripts/main.py
@@ -7,18 +7,18 @@ stock = InventoryManager(base_path)
 
 
 # old test/sample code
-# print(stock.getDate())
-# print("=" * 10) 
-# print(stock.getStock())
-# print("=" * 10)
-# print(stock.getSuppliers())
-# stolen = stock.searchItem("Nic")
-# stock.removeStock(stolen[0].item_id, 38)
-# stolen = stock.searchItem("Nic")
-# stock.dateTicker()
-# print("=" * 15)
-# characters = ["\"", " "]
-# splicer = Splicer(characters, True)
-# commandTest = splicer.splice("add \"Barge Bogs\" 9 there")
-# for i in commandTest:
-#     print(i)
+print(stock.getDate())
+print("=" * 10) 
+print(stock.getStock())
+print("=" * 10)
+print(stock.getSuppliers())
+stolen = stock.searchItem("Nic")
+stock.removeStock(stolen[0].item_id, 38)
+stolen = stock.searchItem("Nic")
+stock.dateTicker()
+print("=" * 15)
+characters = ["\"", " "]
+splicer = Splicer(characters, True)
+commandTest = splicer.splice("add \"Barge Bogs\" 9 there")
+for i in commandTest:
+    print(i)
